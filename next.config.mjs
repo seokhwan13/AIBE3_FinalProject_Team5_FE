c74eea6d357863788d5fb8579609a23b0daed775
seokhwan13 <seokhwan1113@naver.com>
2025-12-08 00:51:29 +0000

    nextConfig env제거

--- next.config.mjs
+++ next.config.mjs
@@ -12,12 +12,12 @@ const nextConfig = {
     unoptimized: true,
   },
   // 클라이언트 번들로 주입할 환경 변수 명시
-  env: {
-    NEXT_PUBLIC_KAKAO_MAP_API_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY,
-    NEXT_PUBLIC_API_BASE_URL: "http://localhost:8080",
-    NEXT_PUBLIC_API_URL: "http://localhost:8080/api/v1",
-    NEXT_PUBLIC_WS_URL: "http://localhost:8080/ws",
-  },
+  // env: {
+  //   NEXT_PUBLIC_KAKAO_MAP_API_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY,
+  //   NEXT_PUBLIC_API_BASE_URL: "http://localhost:8080",
+  //   NEXT_PUBLIC_API_URL: "http://localhost:8080/api/v1",
+  //   NEXT_PUBLIC_WS_URL: "http://localhost:8080/ws",
+  // },
 };
 
 export default nextConfig;
